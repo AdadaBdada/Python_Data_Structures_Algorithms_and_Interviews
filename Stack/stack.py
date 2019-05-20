c4a55090c602396b7034011b63d4daa03f3a4795
Shuo <sjin6@stevens.edu>
2019-05-20 02:22:08 -0400

    implement stack using list

---
+++ Stack/stack.py
@@ -0,0 +1,20 @@
+class Stack(object):
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+
+        return self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
