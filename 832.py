ac4b10a0ab9772d09f14f004a7f6fcdb307a6115
Shuo <sjin6@stevens.edu>
2019-07-02 14:01:45 -0400

    832

--- 832.py
+++ 832.py
@@ -1,9 +1,18 @@
 # 832. Flipping an Image
 
-class Solution:
-    def flipAndInvertImage(self, A):
-        """
-        :type A: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        return [[1^i for i in row[::-1]] for row in A]
+# class Solution:
+
+
+def flipAndInvertImage(self, A):
+    """
+    :type A: List[List[int]]
+    :rtype: List[List[int]]
+    """
+    return [[1 ^ i for i in row[::-1]] for row in A]
+
+
+if __name__ == "__main":
+
+    A = [1, 2, 3, 4]
+
+    print(flipAndInvertImage(A))
